eb5ca2dacd0996e14b11d383867af23c2a9eeb66
Jesús Marqués <j.marquessales@gmail.com>
2020-03-01 00:04:35 +0100

    Tests de integración.

---
+++ app/src/test/java/com/txusmslabs/templateapp/ui/detail/DetailIntegrationTests.kt
@@ -0,0 +1,71 @@
+package com.txusmslabs.templateapp.ui.detail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.txusmslabs.data.source.LocalDataSource
+import com.txusmslabs.domain.Movie
+import com.txusmslabs.templateapp.FakeLocalDataSource
+import com.txusmslabs.templateapp.defaultFakeMovies
+import com.txusmslabs.templateapp.initMockedDi
+import com.txusmslabs.testshared.mockedMovie
+import com.txusmslabs.usecases.FindMovieById
+import com.txusmslabs.usecases.ToggleMovieFavorite
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.parameter.parametersOf
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class DetailIntegrationTests: AutoCloseKoinTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observer: Observer<Movie>
+
+    private lateinit var vm: DetailViewModel
+    private lateinit var localDataSource: FakeLocalDataSource
+
+    @Before
+    fun setUp() {
+        val vmModule = module {
+            factory { (id: Int) -> DetailViewModel(id, get(), get(), get()) }
+            factory { FindMovieById(get()) }
+            factory { ToggleMovieFavorite(get()) }
+        }
+
+        initMockedDi(vmModule)
+        vm = get { parametersOf(1) }
+
+        localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = defaultFakeMovies
+    }
+
+    @Test
+    fun `observing LiveData finds the movie`() {
+        vm.movie.observeForever(observer)
+
+        verify(observer).onChanged(mockedMovie.copy(1))
+    }
+
+    @Test
+    fun `favorite is updated in local data source`() {
+        vm.movie.observeForever(observer)
+
+        vm.onFavoriteClicked()
+
+        runBlocking {
+            assertTrue(localDataSource.findById(1).favorite)
+        }
+    }
+}
---
+++ app/src/test/java/com/txusmslabs/templateapp/ui/main/MainIntegrationTests.kt
@@ -0,0 +1,66 @@
+package com.txusmslabs.templateapp.ui.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.txusmslabs.data.source.LocalDataSource
+import com.txusmslabs.domain.Movie
+import com.txusmslabs.templateapp.FakeLocalDataSource
+import com.txusmslabs.templateapp.defaultFakeMovies
+import com.txusmslabs.templateapp.initMockedDi
+import com.txusmslabs.testshared.mockedMovie
+import com.txusmslabs.usecases.GetPopularMovies
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class MainIntegrationTests: AutoCloseKoinTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observerMovies: Observer<List<Movie>>
+
+    private lateinit var vm: MainViewModel
+
+    @Before
+    fun setUp() {
+        val vmModule = module {
+            factory { MainViewModel(get(), get()) }
+            factory { GetPopularMovies(get()) }
+        }
+
+        initMockedDi(vmModule)
+        vm = get()
+    }
+
+    @Test
+    fun `data is loaded from server when local source is empty`() {
+        vm.movies.observeForever(observerMovies)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observerMovies).onChanged(defaultFakeMovies)
+    }
+
+    @Test
+    fun `data is loaded from local source when available`() {
+        val fakeLocalMovies = listOf(mockedMovie.copy(10), mockedMovie.copy(11))
+        val localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = fakeLocalMovies
+        vm.movies.observeForever(observerMovies)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observerMovies).onChanged(fakeLocalMovies)
+    }
+}
