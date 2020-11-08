c2c60e4307787b377a2c12bbb6047c1e7dc1cac3
Jesús Marqués <j.marquessales@gmail.com>
2020-11-09 00:45:40 +0100

    Migrate to Kotlin DSL.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,2 @@
 include(":app", ":domain", ":data", ":usecases", ":testShared")
-//include("domain")
-//include("data")
-//include("usecases")
-//include("testShared")
 rootProject.name="TemplateApp"
